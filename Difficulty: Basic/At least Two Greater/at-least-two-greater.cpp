b3a7e4a62975c3f78d84163bfec8d3594db3e099
PARTH SINGH <parthsingh1947@gmail.com>
2026-09-10 11:46:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/At least Two Greater/at-least-two-greater.cpp
@@ -0,0 +1,9 @@
+class Solution {
+  public:
+    vector<int> findElements(vector<int> arr) {
+        // code here
+             sort(arr.begin(),arr.end());
+        arr.resize(arr.size()-2);
+        return arr;
+    }
+};
